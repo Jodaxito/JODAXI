1147404be0bf9dd96cf50203610d25a72cbd3edd
Jose Mendoza <al222410838@gmail.com>
2026-04-07 00:37:03 -0600

    Fix: Aumentar límite de body a 50mb para imágenes

--- server/server.js
+++ server/server.js
@@ -7,8 +7,8 @@ const app = express();
 
 // Middleware esenciales PRIMERO
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Middleware para loggear requests (debug)
 app.use((req, res, next) => {
